6e591299deb3db59f7cace276b118b9c83a91277
carolineclark <caroline@kano.me>
2015-11-29 20:19:52 +0000

    Draft first set of tests

--- tests.py
+++ tests.py
@@ -27,61 +27,71 @@
 
 
 import unittest
+from bowling import (
+    BowlingScore, NegativePinError, NotIntegerPinError, TooBigPinError
+)
 
-
-# Data structure of the scores:
-# [
-#   {
-#       "total": 3,
-#       "score": (2, 1)
-#   }
-# ]
 
 class KnownValues(unittest.TestCase):
     known_values = (
         [
             {
                 "total": 5,
-                "score": (1, 4)
+                "pins": (1, 4)
             },
             {
                 "total": 14,
-                "score": (4, 5)
+                "pins": (4, 5)
             },
             {
                 "total": 29,
-                "score": (6, 4)
+                "pins": (6, 4)
             },
             {
                 "total": 49,
-                "score": (5, 5)
+                "pins": (5, 5)
             },
             {
                 "total": 60,
-                "score": (10, 0)
+                "pins": (10, 0)
             },
             {
                 "total": 61,
-                "score": (0, 1)
+                "pins": (0, 1)
             },
             {
                 "total": 77,
-                "score": (7, 3)
+                "pins": (7, 3)
             },
             {
                 "total": 97,
-                "score": (6, 4)
+                "pins": (6, 4)
             },
             {
                 "total": 117,
-                "score": (10, 0)
+                "pins": (10, 0)
             },
             {
                 "total": 133,
-                "score": (2, 8, 6)
+                "pins": (2, 8, 6)
             }
         ]
     )
+
+
+class BadPinValues(unittest.TestCase):
+
+    def pins_negative(self):
+        score = BowlingScore()
+        self.assertRaises(NegativePinError, score.add_pins, (-1, 2))
+
+    def pins_fractional(self):
+        score = BowlingScore()
+        self.assertRaises(NotIntegerPinError, score.add_pins, (0.5, 2))
+
+    def pins_too_many(self):
+        score = BowlingScore()
+        self.assertRaises(TooBigPinError, score.add_pins, (7, 7))
 
 
 if __name__ == "__main__":
