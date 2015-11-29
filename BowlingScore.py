58f72a016e65236bb7940c0ead803bbe99607876
carolineclark <caroline@kano.me>
2015-11-29 21:31:04 +0000

    Started calculating the totals of the pins

--- BowlingScore.py
+++ BowlingScore.py
@@ -24,6 +24,7 @@ class WrongTriesInFrame(BowlingError):
 class TooManyFramesError(BowlingError):
     pass
 
+
 # Data structure of the scores:
 # [
 #   {
@@ -59,12 +60,23 @@ class BowlingScore():
 
         # For the number of frames less than or equal to 9, check the number of
         # tries is less than 2, and no more than 10 pins are knocked down
-        if len(self._score) < 9:
+        if len(self.score) < 9:
             self._add_regular_fetch_score(args)
 
         # Last fetch has slightly different rules
-        elif len(self._score) == 9:
+        elif len(self.score) == 9:
             self._add_last_fetch_score(args)
+
+        self._calculate_last_total()
+
+    def get_last_total(self):
+        '''
+        Return the last one with a total that isn't None
+        '''
+
+        for score in self.score[::-1]:
+            if score["total"]:
+                return score["total"]
 
     def _add_regular_fetch_score(self, pin_scores):
         if not len(pin_scores) == 2:
@@ -73,7 +85,7 @@ class BowlingScore():
             raise TooBigPinError
 
         # Total is None to indicate it needs calculating
-        self._score.append({"pins": pin_scores, "total": None})
+        self.score.append({"pins": pin_scores, "total": None})
 
     def _add_last_fetch_score(self, pin_scores):
         if not len(pin_scores) == 2 or len(pin_scores) == 3:
@@ -81,7 +93,11 @@ class BowlingScore():
         if sum(pin_scores) > 30:
             raise TooBigPinError
 
-        self._score.append({"pins": pin_scores, "total": None})
+        self.score.append({"pins": pin_scores, "total": None})
 
-    def calculate_total(self):
-        pass
+    def _calculate_last_total(self):
+        # Take last section and check total:
+        if self.score[-1]["total"] is None:
+
+            if len(self.score) == 1:
+                self.score[-1]["total"] = sum(self.score[-1]["pins"])
