522ed113d2c1092bcace67151505d2ae365e5dd1
carolineclark <caroline@kano.me>
2015-12-02 09:08:28 +0000

    Bowling scores totals calculated, added more tests

--- BowlingScore.py
+++ BowlingScore.py
@@ -25,6 +25,10 @@ class TooManyFramesError(BowlingError):
     pass
 
 
+class WrongNumberOfFrames(BowlingError):
+    pass
+
+
 # Data structure of the scores:
 # [
 #   {
@@ -34,49 +38,48 @@ class TooManyFramesError(BowlingError):
 # ]
 class BowlingScore():
     def __init__(self):
-        self._score = []
+        self._scores = []
 
     @property
-    def score(self):
-        return self._score
+    def scores(self):
+        return self._scores
 
     def get_totals(self):
-        return [x["total"] for x in self.score]
+        return [x["total"] for x in self.scores]
 
     def get_pins(self):
-        return [x["pins"] for x in self.score]
+        return [x["pins"] for x in self.scores]
 
-    def add_pins(self, *args):
+    def add_all_frames(self, pins):
         '''
-        Add tuple of pin scores.
+        Add all frames into the class.
+        :param pins: list of tuples of pin scores
+        :type pins: list
+        '''
+        for pin in pins:
+            self.add_frame(pin)
+
+    # This is with the attitude of adding one frame at a time
+    def add_frame(self, pin_tuple):
+        '''
+        Add tuple of pin scores, one at a time.
         '''
 
-        if len(self.score) >= 10:
+        if len(self.scores) >= 10:
             raise TooManyFramesError
-        if not all(isinstance(x, int) for x in args):
+        if not all(isinstance(x, int) for x in pin_tuple):
             raise NotIntegerPinError
-        if not all(x >= 0 for x in args):
+        if not all(x >= 0 for x in pin_tuple):
             raise NegativePinError
 
         # For the number of frames less than or equal to 9, check the number of
         # tries is less than 2, and no more than 10 pins are knocked down
-        if len(self.score) < 9:
-            self._add_regular_fetch_score(args)
+        if len(self.scores) < 9:
+            self._add_regular_fetch_score(pin_tuple)
 
         # Last fetch has slightly different rules
-        elif len(self.score) == 9:
-            self._add_last_fetch_score(args)
-
-        self._calculate_last_total()
-
-    def get_last_total(self):
-        '''
-        Return the last one with a total that isn't None
-        '''
-
-        for score in self.score[::-1]:
-            if score["total"]:
-                return score["total"]
+        elif len(self.scores) == 9:
+            self._add_last_fetch_score(pin_tuple)
 
     def _add_regular_fetch_score(self, pin_scores):
         if not len(pin_scores) == 2:
@@ -85,19 +88,52 @@ class BowlingScore():
             raise TooBigPinError
 
         # Total is None to indicate it needs calculating
-        self.score.append({"pins": pin_scores, "total": None})
+        self.scores.append({"pins": pin_scores, "total": None})
 
     def _add_last_fetch_score(self, pin_scores):
-        if not len(pin_scores) == 2 or len(pin_scores) == 3:
+        if not (len(pin_scores) == 2 or len(pin_scores) == 3):
             raise WrongTriesInFrame
         if sum(pin_scores) > 30:
             raise TooBigPinError
 
-        self.score.append({"pins": pin_scores, "total": None})
+        self.scores.append({"pins": pin_scores, "total": None})
 
-    def _calculate_last_total(self):
-        # Take last section and check total:
-        if self.score[-1]["total"] is None:
+    # Assumes you have all the pin scores
+    def calculate_all_totals(self):
+        self._check_number_of_frames()
 
-            if len(self.score) == 1:
-                self.score[-1]["total"] = sum(self.score[-1]["pins"])
+        prev_total = 0
+        for i in range(9):
+            pins = self._scores[i]["pins"]
+            future_pins = self._scores[i + 1]["pins"]
+            self._scores[i]["total"] = self._calculate_non_final_total(
+                prev_total, pins, future_pins
+            )
+            prev_total = self._scores[i]["total"]
+
+        pins = self._scores[-1]["pins"]
+        self._scores[-1]["total"] = self._calculate_final_total(
+            prev_total, pins
+        )
+
+    def _check_number_of_frames(self):
+        if not len(self._scores) == 10:
+            raise WrongNumberOfFrames
+
+    # This is for non final totals
+    def _calculate_non_final_total(self, prev_total, pin_score,
+                                   future_pin_score):
+
+        pin_total = sum(pin_score)
+
+        if pin_total < 10:
+            return prev_total + pin_total
+        elif 10 in pin_score:
+            return prev_total + pin_total + future_pin_score[0] + \
+                future_pin_score[1]
+        else:
+            return prev_total + pin_total + future_pin_score[0]
+
+    # Is this that simple?
+    def _calculate_final_total(self, prev_total, pin_score):
+        return prev_total + sum(pin_score)
--- tests.py
+++ tests.py
@@ -34,87 +34,128 @@ from BowlingScore import (
 
 
 class KnownValues(unittest.TestCase):
-    known_values = (
-        [
-            {
-                "total": 5,
-                "pins": (1, 4)
-            },
-            {
-                "total": 14,
-                "pins": (4, 5)
-            },
-            {
-                "total": 29,
-                "pins": (6, 4)
-            },
-            {
-                "total": 49,
-                "pins": (5, 5)
-            },
-            {
-                "total": 60,
-                "pins": (10, 0)
-            },
-            {
-                "total": 61,
-                "pins": (0, 1)
-            },
-            {
-                "total": 77,
-                "pins": (7, 3)
-            },
-            {
-                "total": 97,
-                "pins": (6, 4)
-            },
-            {
-                "total": 117,
-                "pins": (10, 0)
-            },
-            {
-                "total": 133,
-                "pins": (2, 8, 6)
-            }
-        ]
-    )
+
+    known_values = {
+        # No bonus
+        "no-bonus": {
+            "pins": [
+                (1, 1),
+                (1, 2),
+                (2, 5),
+                (3, 5),
+                (7, 1),
+                (4, 4),
+                (5, 4),
+                (9, 0),
+                (0, 0),
+                (0, 2)
+            ],
+            "totals": [2, 5, 12, 20, 28, 36, 45, 54, 54, 56]
+        },
+        # Basic bonus
+        "basic-bonus": {
+            "pins": [
+                (1, 1),
+                (1, 9),
+                (2, 5),
+                (3, 5),
+                (7, 1),
+                (4, 4),
+                (5, 4),
+                (9, 0),
+                (0, 0),
+                (0, 2)
+            ],
+            "totals": [2, 14, 21, 29, 37, 45, 54, 63, 63, 65]
+        },
+        # Strike
+        "strike": {
+            "pins": [
+                (1, 1),
+                (1, 2),
+                (2, 5),
+                (3, 5),
+                (7, 1),
+                (4, 4),
+                (5, 4),
+                (10, 0),
+                (1, 0),
+                (0, 2)
+            ],
+            "totals": [2, 5, 12, 20, 28, 36, 45, 56, 57, 59]
+        },
+        # Example from email
+        "email-example": {
+            "pins": [
+                (1, 4),
+                (4, 5),
+                (6, 4),
+                (5, 5),
+                (10, 0),
+                (0, 1),
+                (7, 3),
+                (6, 4),
+                (10, 0),
+                (2, 8, 6)
+            ],
+            "totals": [5, 14, 29, 49, 60, 61, 77, 97, 117, 133]
+        }
+    }
+
+    def test_no_bonus(self):
+        pins = self.known_values["no-bonus"]["pins"]
+        totals = self.known_values["no-bonus"]["totals"]
+        self.pins_vs_totals(pins, totals)
+
+    def test_basic_bonus(self):
+        pins = self.known_values["basic-bonus"]["pins"]
+        totals = self.known_values["basic-bonus"]["totals"]
+        self.pins_vs_totals(pins, totals)
+
+    def test_strike(self):
+        pins = self.known_values["strike"]["pins"]
+        totals = self.known_values["strike"]["totals"]
+        self.pins_vs_totals(pins, totals)
+
+    def test_email_example(self):
+        pins = self.known_values["email-example"]["pins"]
+        totals = self.known_values["email-example"]["totals"]
+        self.pins_vs_totals(pins, totals)
+
+    def pins_vs_totals(self, pins, totals):
+        score = BowlingScore()
+        score.add_all_frames(pins)
+        score.calculate_all_totals()
+        self.assertEquals(score.get_totals(), totals)
 
 
 class BadPinValues(unittest.TestCase):
 
     def test_negative(self):
         score = BowlingScore()
-        self.assertRaises(NegativePinError, score.add_pins, -1, 2)
+        self.assertRaises(NegativePinError, score.add_frame, (-1, 2))
 
     def test_fractional(self):
         score = BowlingScore()
-        self.assertRaises(NotIntegerPinError, score.add_pins, 0.5, 2)
+        self.assertRaises(NotIntegerPinError, score.add_frame, (0.5, 2))
 
     def test_big_total(self):
         score = BowlingScore()
-        self.assertRaises(TooBigPinError, score.add_pins, 7, 7)
+        self.assertRaises(TooBigPinError, score.add_frame, (7, 7))
 
     def test_too_many_tries(self):
         score = BowlingScore()
-        self.assertRaises(WrongTriesInFrame, score.add_pins, 1, 1, 1)
+        self.assertRaises(WrongTriesInFrame, score.add_frame, (1, 1, 1))
 
     def test_too_few_tries(self):
         score = BowlingScore()
-        self.assertRaises(WrongTriesInFrame, score.add_pins, 1)
+        self.assertRaises(WrongTriesInFrame, score.add_frame, (1,))
 
     def test_too_many_fetches(self):
         score = BowlingScore()
         for i in range(10):
-            score.add_pins(1, 1)
-        self.assertRaises(TooManyFramesError, score.add_pins, 1, 1)
-
-
-class BonusPinTests(unittest.TestCase):
-
-    def test_no_bonus(self):
-        score = BowlingScore()
-        score.add_pins(2, 1)
-        self.assertEqual(score.get_last_total(), 3)
+            score.add_frame((1, 1))
+        self.assertRaises(TooManyFramesError, score.add_frame, (1, 1))
 
 
 if __name__ == "__main__":
